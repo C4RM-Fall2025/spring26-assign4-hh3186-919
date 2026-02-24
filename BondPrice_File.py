78515944d7e9c15d68e47f8fde63595350b1b186
hh3186-919 <hh3186@columbia.edu>
2026-02-23 20:59:51 -0500

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -6,5 +6,5 @@ def getBondPrice(y, face, couponRate, m, ppy=1):
     price = 0.0
     for t in range(1, n + 1):
         price += c / ((1 + r) ** t)
-    price += face / ((1 + r) ** n)
+        price += face / ((1 + r) ** n)
     return price
